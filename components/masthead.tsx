3556380b98908a187da0eba7bc24518f8c389137
AShefkiu <arditshefkiu@gmail.com>
2024-02-07 11:44:16 +0100

    initial commit

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 import { ScrollContext } from "../utils/scroll-observer";
 
 const Masthead: React.FC = () => {
-const [imageLoaded,setImageLoaded] = useState(false)
-    const refContainer = useRef<HTMLDivElement>(null);
-    const {scrollY} = useContext(ScrollContext)
-    let progress = 0 
-    const {current:elContainer} = refContainer
-    if(elContainer){
-        progress = Math.min(1,scrollY / elContainer.clientHeight)
-    }
+  const [imageLoaded, setImageLoaded] = useState(false)
+  const refContainer = useRef<HTMLDivElement>(null);
+  const { scrollY } = useContext(ScrollContext)
+  let progress = 0
+  const { current: elContainer } = refContainer
+  if (elContainer) {
+    progress = Math.min(1, scrollY / elContainer.clientHeight)
+  }
 
-    const handleImageLoaded = useCallback(() => {
-        setImageLoaded(true)
-    },[]) 
+  const handleImageLoaded = useCallback(() => {
+    setImageLoaded(true)
+  }, [])
 
   return (
     <div
@@ -34,7 +34,7 @@ const [imageLoaded,setImageLoaded] = useState(false)
         <source src="/assets/masthead.webm" type="video/webm; codecs=vp9" />
       </video>
       <div className={`flex-grow-0 pt-10 transition-opacity duration-1000 ${imageLoaded ? 'opacity-100' : 'opacity-0'}`}>
-          <Image src={"/assets/ubt-logo.png"}  width={128/3} height={114/3} alt="logo" />
+        <Image src={"/assets/ubt-logo.png"} width={128 / 3} height={114 / 3} alt="logo" />
       </div>
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] 
             text-center flex-1 flex items-center justify-center flex-col">
@@ -44,15 +44,15 @@ const [imageLoaded,setImageLoaded] = useState(false)
         </h2>
       </div>
       <div className={`flex-grow-0 pb-20 md:pb-10 transition-all duration-1000
-      ${imageLoaded ? 'opacity-100' : 'opacity-0 -translate-y-10' }`}>
-          <Image
+      ${imageLoaded ? 'opacity-100' : 'opacity-0 -translate-y-10'}`}>
+        <Image
           src={"/assets/arrow-down.png"}
-          width={188/3}
-          height={105/3} 
-          alt="scroll down" 
+          width={188 / 3}
+          height={105 / 3}
+          alt="scroll down"
           onLoadingComplete={handleImageLoaded} />
       </div>
-       </div>
+    </div>
   )
 }
 export default Masthead
